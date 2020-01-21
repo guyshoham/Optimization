a85d616afbaa6366fb13659493abaaf142fb5966
tom5012 <tsanderovich@gmail.com>
2020-01-21 18:48:32 +0200

    rotate function works properly. smooth is not working yet

--- kernels.c
+++ kernels.c
@@ -8,13 +8,13 @@
  * Please fill in the following team struct 
  */
 team_t team = {
-    "GuyTom",              /* Team name */
+        "GuyTom",              /* Team name */
 
-    "Guy Shoham",     /* First member full name */
-    "GuyShoham28@gmail.com",  /* First member email address */
+        "Guy Shoham",     /* First member full name */
+        "GuyShoham28@gmail.com",  /* First member email address */
 
-    "",                   /* Second member full name (leave blank if none) */
-    ""                    /* Second member email addr (leave blank if none) */
+        "Tom Sendrovich",                   /* Second member full name (leave blank if none) */
+        "tsanderovich@gmail.com"                    /* Second member email addr (leave blank if none) */
 };
 
 /***************
@@ -29,21 +29,42 @@ team_t team = {
  * naive_rotate - The naive baseline version of rotate 
  */
 char naive_rotate_descr[] = "naive_rotate: Naive baseline implementation";
-void naive_rotate(int dim, pixel* src, pixel* dst) {
-  int i, j;
 
-  for (i = 0; i < dim; i++)
-    for (j = 0; j < dim; j++)
-      dst[RIDX(dim - 1 - j, i, dim)] = src[RIDX(i, j, dim)];
+void naive_rotate(int dim, pixel *src, pixel *dst) {
+    int i, j;
+
+    for (i = 0; i < dim; i++)
+        for (j = 0; j < dim; j++)
+            dst[RIDX(dim - 1 - j, i, dim)] = src[RIDX(i, j, dim)];
 }
+
+
+void my_rotate(int dim, pixel *src, pixel *dst) {
+    int cacheSize = 32, j, i, col, row, inCol, inRow;
+
+    for (i = 0; i < dim; i = i + cacheSize) {
+        for (j = 0; j < dim; j = j + cacheSize) {
+            inCol = j + cacheSize;
+            //by switching the two loops we get better spatial locality (more cache hits!)
+            for (col = j; col < inCol; col++) {//goes over a box of 32X32 pixels at a time
+                inRow = i + cacheSize;
+                for (row = i; row < inRow; row++) {
+                    dst[RIDX(dim - 1 - col, row, dim)] = src[RIDX(row, col, dim)];
+                }
+            }
+        }
+    }
+}
+
 
 /* 
  * rotate - Your current working version of rotate
  * IMPORTANT: This is the version you will be graded on
  */
-char rotate_descr[] = "rotate: Current working version";
-void rotate(int dim, pixel* src, pixel* dst) {
-  naive_rotate(dim, src, dst);
+char my_rotate_descr[] = "rotate: Current working version";
+
+void rotate(int dim, pixel *src, pixel *dst) {
+    my_rotate(dim, src, dst);
 }
 
 /*********************************************************************
@@ -55,9 +76,9 @@ void rotate(int dim, pixel* src, pixel* dst) {
  *********************************************************************/
 
 void register_rotate_functions() {
-  add_rotate_function(&naive_rotate, naive_rotate_descr);
-  add_rotate_function(&rotate, rotate_descr);
-  /* ... Register additional test functions here */
+    add_rotate_function(&naive_rotate, naive_rotate_descr);
+    add_rotate_function(&rotate, my_rotate_descr);
+    /* ... Register additional test functions here */
 }
 
 
@@ -72,62 +93,63 @@ void register_rotate_functions() {
 
 /* A struct used to compute averaged pixel value */
 typedef struct {
-  int red;
-  int green;
-  int blue;
-  int num;
+    int red;
+    int green;
+    int blue;
+    int num;
 } pixel_sum;
 
 /* Compute min and max of two integers, respectively */
 static int min(int a, int b) { return (a < b ? a : b); }
+
 static int max(int a, int b) { return (a > b ? a : b); }
 
 /* 
  * initialize_pixel_sum - Initializes all fields of sum to 0 
  */
-static void initialize_pixel_sum(pixel_sum* sum) {
-  sum->red = sum->green = sum->blue = 0;
-  sum->num = 0;
-  return;
+static void initialize_pixel_sum(pixel_sum *sum) {
+    sum->red = sum->green = sum->blue = 0;
+    sum->num = 0;
+    return;
 }
 
 /* 
  * accumulate_sum - Accumulates field values of p in corresponding 
  * fields of sum 
  */
-static void accumulate_sum(pixel_sum* sum, pixel p) {
-  sum->red += (int) p.red;
-  sum->green += (int) p.green;
-  sum->blue += (int) p.blue;
-  sum->num++;
-  return;
+static void accumulate_sum(pixel_sum *sum, pixel p) {
+    sum->red += (int) p.red;
+    sum->green += (int) p.green;
+    sum->blue += (int) p.blue;
+    sum->num++;
+    return;
 }
 
 /* 
  * assign_sum_to_pixel - Computes averaged pixel value in current_pixel 
  */
-static void assign_sum_to_pixel(pixel* current_pixel, pixel_sum sum) {
-  current_pixel->red = (unsigned short) (sum.red / sum.num);
-  current_pixel->green = (unsigned short) (sum.green / sum.num);
-  current_pixel->blue = (unsigned short) (sum.blue / sum.num);
-  return;
+static void assign_sum_to_pixel(pixel *current_pixel, pixel_sum sum) {
+    current_pixel->red = (unsigned short) (sum.red / sum.num);
+    current_pixel->green = (unsigned short) (sum.green / sum.num);
+    current_pixel->blue = (unsigned short) (sum.blue / sum.num);
+    return;
 }
 
 /* 
  * avg - Returns averaged pixel value at (i,j) 
  */
-static pixel avg(int dim, int i, int j, pixel* src) {
-  int ii, jj;
-  pixel_sum sum;
-  pixel current_pixel;
+static pixel avg(int dim, int i, int j, pixel *src) {
+    int ii, jj;
+    pixel_sum sum;
+    pixel current_pixel;
 
-  initialize_pixel_sum(&sum);
-  for (ii = max(i - 1, 0); ii <= min(i + 1, dim - 1); ii++)
-    for (jj = max(j - 1, 0); jj <= min(j + 1, dim - 1); jj++)
-      accumulate_sum(&sum, src[RIDX(ii, jj, dim)]);
+    initialize_pixel_sum(&sum);
+    for (ii = max(i - 1, 0); ii <= min(i + 1, dim - 1); ii++)
+        for (jj = max(j - 1, 0); jj <= min(j + 1, dim - 1); jj++)
+            accumulate_sum(&sum, src[RIDX(ii, jj, dim)]);
 
-  assign_sum_to_pixel(&current_pixel, sum);
-  return current_pixel;
+    assign_sum_to_pixel(&current_pixel, sum);
+    return current_pixel;
 }
 
 /******************************************************
@@ -138,22 +160,43 @@ static pixel avg(int dim, int i, int j, pixel* src) {
  * naive_smooth - The naive baseline version of smooth 
  */
 char naive_smooth_descr[] = "naive_smooth: Naive baseline implementation";
-void naive_smooth(int dim, pixel* src, pixel* dst) {
-  int i, j;
 
-  for (i = 0; i < dim; i++)
-    for (j = 0; j < dim; j++)
-      dst[RIDX(i, j, dim)] = avg(dim, i, j, src);
+void naive_smooth(int dim, pixel *src, pixel *dst) {
+    int i, j;
+
+    for (i = 0; i < dim; i++)
+        for (j = 0; j < dim; j++)
+            dst[RIDX(i, j, dim)] = avg(dim, i, j, src);
 }
+
+
+void my_smooth(int dim, pixel *src, pixel *dst) {
+    int cacheSize = 32, j, i, col, row, inCol, inRow;
+
+    for (i = 0; i < dim; i = i + cacheSize) {
+        for (j = 0; j < dim; j = j + cacheSize) {
+            inCol = j + cacheSize;
+            for (col = j; col < inCol; col++) {
+                inRow = i + cacheSize;
+                for (row = i; row < inRow; row++) {
+                    dst[RIDX(dim - 1 - col, row, dim)] = avg(dim, col, row, src);
+                }
+            }
+        }
+    }
+}
+
+
 
 /*
  * smooth - Your current working version of smooth. 
  * IMPORTANT: This is the version you will be graded on
  */
-char smooth_descr[] = "smooth: Current working version";
-void smooth(int dim, pixel* src, pixel* dst) {
-  naive_smooth(dim, src, dst);
-}
+        char smooth_descr[] = "smooth: Current working version";
+
+        void smooth(int dim, pixel *src, pixel *dst) {
+            my_smooth(dim, src, dst);
+        }
 
 /*********************************************************************
  * register_smooth_functions - Register all of your different versions
@@ -163,9 +206,9 @@ void smooth(int dim, pixel* src, pixel* dst) {
  *     registered test function.  
  *********************************************************************/
 
-void register_smooth_functions() {
-  add_smooth_function(&smooth, smooth_descr);
-  add_smooth_function(&naive_smooth, naive_smooth_descr);
-  /* ... Register additional test functions here */
-}
+        void register_smooth_functions() {
+            add_smooth_function(&smooth, smooth_descr);
+            add_smooth_function(&naive_smooth, naive_smooth_descr);
+            /* ... Register additional test functions here */
+        }
 
